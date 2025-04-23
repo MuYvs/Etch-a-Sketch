da98e706cd5acafd480a2cc602544b24d4a6270f
MuYvs <MuhammadYvsvf@gmail.com>
2025-04-23 21:12:41 +0200

    Added erase functionality to rmb

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let grid_container = document.querySelector(".grid-container");
 let grid_slider = document.querySelector("#grid-slider");
 let grid_size_text = document.querySelector(".grid-size-text");
+let game_container = document.querySelector(".game-container");
 
 function showGridSize(e) {
   let grid_size = e.target.value;
@@ -36,6 +37,7 @@ grid_slider.addEventListener("input", showGridSize);
 grid_slider.addEventListener("input", addGridSquares);
 
 let shouldDraw = false;
+let shouldErase = false;
 function draw(e) {
   let target_box = e.target;
   target_box.style.backgroundColor = "black";
@@ -46,16 +48,29 @@ function erase(e) {
   target_box.style.backgroundColor = "transparent";
 }
 
-document.addEventListener("mousedown", () => {
-  shouldDraw = true;
+document.addEventListener("mousedown", (e) => {
+  if (e.button === 0) {
+    shouldDraw = true;
+  } else if (e.button === 2) {
+    shouldErase = true;
+  }
 });
 
 document.addEventListener("mouseup", () => {
   shouldDraw = false;
+  shouldErase = false;
+});
+
+game_container.addEventListener("dragstart", (e) => {
+  e.preventDefault();
 });
 
 grid_container.addEventListener("mousedown", (e) => {
-  draw(e);
+  if (e.button === 0) {
+    draw(e);
+  } else if (e.button === 2) {
+    erase(e);
+  }
 });
 
 grid_container.addEventListener("dragstart", (e) => {
@@ -65,5 +80,7 @@ grid_container.addEventListener("dragstart", (e) => {
 grid_container.addEventListener("mouseover", (e) => {
   if (shouldDraw) {
     draw(e);
+  } else if (shouldErase) {
+    erase(e);
   }
 });
