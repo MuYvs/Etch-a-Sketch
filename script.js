1dc064f6683d104f39f86228531854871ff7d5f0
MuYvs <MuhammadYvsvf@gmail.com>
2025-04-24 04:19:07 +0200

    Added redo|undo functions with keyboard shortcuts

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let grid_container = document.querySelector(".grid-container");
 let grid_slider = document.querySelector("#grid-slider");
 let grid_size_text = document.querySelector(".grid-size-text");
 let game_container = document.querySelector(".game-container");
+let buttons_container = document.querySelector(".buttons-container");
 
+//Boxes
 function showGridSize(e) {
   let grid_size = e.target.value;
   grid_size_text.textContent = `Grid Size: ${grid_size} x ${grid_size}`;
@@ -36,31 +38,87 @@ function addGridSquares(e) {
 grid_slider.addEventListener("input", showGridSize);
 grid_slider.addEventListener("input", addGridSquares);
 
+//Drawing
 let shouldDraw = false;
 let shouldErase = false;
-function draw(e) {
+
+function draw(e, color) {
   let target_box = e.target;
-  target_box.style.backgroundColor = "black";
+  let prev_color = target_box.style.backgroundColor;
+  let new_color = color;
+
+  if (prev_color != new_color) {
+    target_box.style.backgroundColor = `${color}`;
+    currentStroke.push({ target_box, prev_color, new_color });
+    hasDrawn = true;
+  }
 }
 
-function erase(e) {
-  let target_box = e.target;
-  target_box.style.backgroundColor = "transparent";
-}
-
-document.addEventListener("mousedown", (e) => {
+grid_container.addEventListener("mousedown", (e) => {
   if (e.button === 0) {
     shouldDraw = true;
+    currentStroke = [];
+    draw(e, "black");
   } else if (e.button === 2) {
     shouldErase = true;
+    currentStroke = [];
+    draw(e, "transparent");
   }
 });
 
 document.addEventListener("mouseup", () => {
+  if ((shouldDraw || shouldErase) && hasDrawn) {
+    historyStack.push([...currentStroke]);
+    redoStack.length = 0;
+  }
   shouldDraw = false;
   shouldErase = false;
+  currentStroke = [];
+  hasDrawn = false;
 });
 
+grid_container.addEventListener("mouseover", (e) => {
+  if (shouldDraw) {
+    draw(e, "black");
+  } else if (shouldErase) {
+    draw(e, "transparent");
+  }
+});
+
+// Do | Undo functions
+let currentStroke = [];
+let historyStack = [];
+let redoStack = [];
+let hasDrawn = false;
+
+function undo() {
+  if (historyStack.length === 0) return;
+  let lastStroke = historyStack.pop();
+
+  lastStroke.forEach(({ target_box, prev_color }) => {
+    target_box.style.backgroundColor = prev_color;
+  });
+  redoStack.push(lastStroke);
+}
+
+function redo() {
+  if (redoStack.length === 0) return;
+  let stroke = redoStack.pop();
+
+  stroke.forEach(({ target_box, new_color }) => {
+    target_box.style.backgroundColor = new_color;
+  });
+  historyStack.push(stroke);
+}
+
+document.addEventListener("keydown", (e) => {
+  if (e.ctrlKey && (e.key === "z" || e.key === "Z")) {
+    undo();
+  } else if (e.ctrlKey && (e.key === "y" || e.key === "Y")) {
+    redo();
+  }
+});
+// better ux
 game_container.addEventListener("dragstart", (e) => {
   e.preventDefault();
 });
@@ -69,22 +127,19 @@ game_container.addEventListener("contextmenu", (e) => {
   e.preventDefault();
 });
 
-grid_container.addEventListener("mousedown", (e) => {
-  if (e.button === 0) {
-    draw(e);
-  } else if (e.button === 2) {
-    erase(e);
-  }
-});
-
 grid_container.addEventListener("dragstart", (e) => {
   e.preventDefault();
 });
 
-grid_container.addEventListener("mouseover", (e) => {
-  if (shouldDraw) {
-    draw(e);
-  } else if (shouldErase) {
-    erase(e);
+buttons_container.addEventListener("click", (e) => {
+  let target_button = e.target;
+
+  switch (target_button.id) {
+    case "btn-next":
+      redo();
+      return;
+    case "btn-reverse":
+      undo();
+      return;
   }
 });
